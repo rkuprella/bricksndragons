727d76d934219117d70520891ca9819fef0060a1
Pollock Rockstaedter <robinkuprella@gmail.com>
2023-08-18 14:41:38 +0200

    updated missing part 30241

--- pages/index.vue
+++ pages/index.vue
@@ -13,88 +13,77 @@ export default {
       features: [
         {
           title: "Connectable",
-          text:
-            "Wall modules can easily be plugged together via side studs. When reconnected in any figure, floor plates are kept in place.",
-          imgUrl: "/images/connectable.png"
+          text: "Wall modules can easily be plugged together via side studs. When reconnected in any figure, floor plates are kept in place.",
+          imgUrl: "/images/connectable.png",
         },
         {
           title: "Durability over aesthetics",
-          text:
-            "Since modules might get rearranged and shoved around, the usage of loose or easily torn off parts is reduced to a minimum.",
-          imgUrl: "/images/durable.png"
+          text: "Since modules might get rearranged and shoved around, the usage of loose or easily torn off parts is reduced to a minimum.",
+          imgUrl: "/images/durable.png",
         },
         {
           title: "Common parts",
-          text:
-            "Most parts used are common LEGO® bricks. Very few modules contain rare or expensive parts and are labeled as such.",
-          imgUrl: "/images/common.png"
+          text: "Most parts used are common LEGO® bricks. Very few modules contain rare or expensive parts and are labeled as such.",
+          imgUrl: "/images/common.png",
         },
         {
           title: "Reusable parts",
-          text:
-            "No need to rebuy base plates for every theme. All wall modules use the same base plates.",
-          imgUrl: "/images/reusable.png"
+          text: "No need to rebuy base plates for every theme. All wall modules use the same base plates.",
+          imgUrl: "/images/reusable.png",
         },
         {
           title: "2x2 grid",
-          text:
-            "Floor plates used in all modules are always of the same type and color - to display the 2x2 grid. Feel free to customize!",
-          imgUrl: "/images/grid.png"
+          text: "Floor plates used in all modules are always of the same type and color - to display the 2x2 grid. Feel free to customize!",
+          imgUrl: "/images/grid.png",
         },
         {
           title: "Customizable",
-          text:
-            "Of course, any module can easily be extended and modified. It's still LEGO® bricks!",
-          imgUrl: "/images/custom.png"
-        }
+          text: "Of course, any module can easily be extended and modified. It's still LEGO® bricks!",
+          imgUrl: "/images/custom.png",
+        },
       ],
       content: [
         {
           title: "Welcome to Bricks & Dragons",
-          text:
-            "This web app provides instructions and part lists for custom built LEGO® dungeon modules. By copying the XML data of your wanted list, you can easily order the needed LEGO® bricks from bricklink."
+          text: "This web app provides instructions and part lists for custom built LEGO® dungeon modules. By copying the XML data of your wanted list, you can easily order the needed LEGO® bricks from bricklink.",
         },
         {
           imgUrl: "/images/dungeon2.png",
           title: "Modular Brick Dungeons",
-          text:
-            "Build your own modular dungeon and choose from many different themes like castle, cave or tavern.",
+          text: "Build your own modular dungeon and choose from many different themes like castle, cave or tavern.",
           link: {
             title: "All dungeons",
-            to: "/dungeons"
-          }
+            to: "/dungeons",
+          },
         },
         {
           imgUrl: "/images/modules2.png",
           title: "Microscale Modules",
-          text:
-            'The system is built around a 2x2 studs grid (15.8mm). Common battle map tile size is 1" or 25.4mm, which would correspond closer to a 3x3 or 4x4 grid. Please be aware that standard RPG miniatures or LEGO® minifigures do not fit, unless the grid size is extended (which is possible).',
+          text: 'The system is built around a 2x2 studs grid (15.8mm). Common battle map tile size is 1" or 25.4mm, which would correspond closer to a 3x3 or 4x4 grid. Please be aware that standard RPG miniatures or LEGO® minifigures do not fit, unless the grid size is extended (which is possible).',
           link: {
             title: "All modules",
-            to: "/modules"
-          }
+            to: "/modules",
+          },
         },
         {
           imgUrl: "/images/beholders.png",
           title: "Monsters & Heroes",
-          text:
-            "Brickbuilt microscale monsters and buyable LEGO® Heroica microfigures.",
+          text: "Brickbuilt microscale monsters and buyable LEGO® Heroica microfigures.",
           link: {
             title: "All creatures",
-            to: "/creatures"
-          }
-        }
+            to: "/creatures",
+          },
+        },
       ],
       outro: [
         {
           // imgUrl: "/images/allmodules.png",
           title: "Become a Brick Dungeon Master",
-          text:
-            "More content to come in the future. Feel free to join our Discord to share your thoughts, ideas and feedback!",
-          outro: true
-        }
-      ]
+          text: "More content to come in the future. Feel free to join our Discord to share your thoughts, ideas and feedback!",
+          outro: true,
+        },
+      ],
     };
-  }
+  },
 };
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,67 +1,72 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  mode: "jit",
-  purge: [],
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+  ],
   darkMode: "class",
   theme: {
     extend: {
       fontFamily: {
         sans: ["Poppins", "system-ui", "sans-serif"],
-        display: ["Bangers", "system-ui", "sans-serif"]
+        display: ["Bangers", "system-ui", "sans-serif"],
       },
       colors: {
         primary: colors.amber,
         gray: colors.stone,
-        secondary: colors.orange
+        secondary: colors.orange,
       },
-      typography: theme => ({
+      typography: (theme) => ({
         DEFAULT: {
           css: {
             color: "#333",
             a: {
               color: "#1d4ed8",
               "&:hover": {
-                color: "#2563eb"
-              }
+                color: "#2563eb",
+              },
             },
             h1: {
-              color: "#1d4ed8"
+              color: "#1d4ed8",
             },
             h2: {
               color: "#1d4ed8",
-              fontSize: "2.5rem"
+              fontSize: "2.5rem",
             },
             h3: {
               color: "#1d4ed8",
               fontWeight: "400",
-              fontSize: "2rem"
+              fontSize: "2rem",
             },
             h4: {
               color: "#a16207",
               fontWeight: "400",
               // fontFamily: "Bangers",
-              fontSize: "2rem"
+              fontSize: "2rem",
             },
             h5: {
-              color: "#a16207"
+              color: "#a16207",
             },
             h6: {
-              color: "#a16207"
+              color: "#a16207",
             },
             strong: {
-              color: "#222"
+              color: "#222",
             },
             "ul > li::before": {
               width: "0",
               height: "0",
               top: "auto",
-              left: "auto"
+              left: "auto",
             },
             "ul > li": {
-              paddingLeft: "0"
-            }
-          }
+              paddingLeft: "0",
+            },
+          },
         },
         dark: {
           css: {
@@ -69,39 +74,36 @@ module.exports = {
             a: {
               color: "#93c5fd",
               "&:hover": {
-                color: "#bfdbfe"
-              }
+                color: "#bfdbfe",
+              },
             },
             h1: {
-              color: "#60a5fa"
+              color: "#60a5fa",
             },
             h2: {
-              color: "#60a5fa"
+              color: "#60a5fa",
             },
             h3: {
               color: "#60a5fa",
               fontWeight: "400",
-              fontSize: "2rem"
+              fontSize: "2rem",
             },
             h4: {
-              color: "#eab308"
+              color: "#eab308",
             },
             h5: {
-              color: "#eab308"
+              color: "#eab308",
             },
             h6: {
-              color: "#eab308"
+              color: "#eab308",
             },
             strong: {
-              color: "#ddd"
-            }
-          }
-        }
-      })
-    }
+              color: "#ddd",
+            },
+          },
+        },
+      }),
+    },
   },
-  variants: {
-    extend: {}
-  },
-  plugins: [require("@tailwindcss/typography")]
+  plugins: [require("@tailwindcss/typography")],
 };
